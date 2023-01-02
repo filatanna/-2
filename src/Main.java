0d93c83d5a6289440858ec225487bb0e343e14ca
User <filataanna@mail.ru>
2023-01-02 19:46:21 +0300

    задача 1-2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        task1();
+        task2();
+
     }
+    public static void task1() {
+        System.out.println("Здадача1");
+        byte a = 53;
+        System.out.println("Значение переменной а с типом byte равно "+ a);
+        short b = 23000;
+        System.out.println("Значение переменной b с типом short равно "+ b);
+        int c = 2000000;
+        System.out.println("Значение переменной c с типом int равно "+ c);
+        long d = 13000000L;
+        System.out.println("Значение переменной а с типом long равно "+ d);
+        float k = 1.35f;
+        System.out.println("Значение переменной k с типом float равно "+ k);
+        double w = 867.966;
+        System.out.println("Значение переменной w с типом double равно "+ w);
+    }
+    public static  void task2() {
+        System.out.println("Здадача2");
+        float one = 27.12f;
+        long two = 987678965549L;
+        float f = 2.786f;
+        short s = 569;
+        short x = -159;
+        short y = 27897;
+        byte z = 67;
+        System.out.println(one);
+        System.out.println(two);
+        System.out.println(f);
+        System.out.println(s);
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        }
 }
